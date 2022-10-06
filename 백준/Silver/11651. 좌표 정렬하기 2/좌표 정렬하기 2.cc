ffe0e33229c66d60e94b1c387d95def6cb74a2b2
nns503 <73779581+nns503@users.noreply.github.com>
2022-10-06 15:17:36 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 120 ms, Memory: 2676 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+// 좌표 구조 
+typedef struct Point{
+	int x; // x
+	int y; // y
+}P;
+
+// compare 재정의  
+int compare(Point p1, Point p2){
+	if(p1.y==p2.y){
+		return p1.x<p2.x;
+	}
+	
+	return p1.y < p2.y;
+}
+
+int main(void){
+	using namespace std;
+	
+	int n;
+	cin >> n;
+	
+	P point[n];
+
+	
+	for(int i=0;i<n;i++){
+		cin >> point[i].x >> point[i].y;
+	}
+	
+	sort(point,point+n,compare);
+	
+	for(int i=0;i<n;i++){ 
+		cout << point[i].x << " " << point[i].y << "\n" ;	
+	}
+	
+	return 0;
+}
