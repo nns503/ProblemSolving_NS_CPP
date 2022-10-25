b8dc98911011773668f4451f50ee218a69633e40
nns503 <73779581+nns503@users.noreply.github.com>
2022-10-25 11:26:49 +0900

    [Silver IV] Title: 괄호, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main(){
+	
+	stack<char> st;
+	int n;
+	string a;
+	int x=0,y=0;
+	cin >> n;
+	
+	
+	for(int i=0;i<n;i++){
+		cin >> a;
+		for(int j=0;j<a.size();j++){
+			st.push(a[j]);
+		}
+		while(st.empty()==0){
+			if(')'== st.top()) x++;
+			else if('('== st.top()) x--;
+			
+			if(x<0){
+				while(st.empty()==0){
+					st.pop();
+				}
+			} 
+			else st.pop();
+		}
+		
+		if(x==0) cout << "YES\n";
+		else cout << "NO\n";
+		
+		x=0;
+	}
+}
