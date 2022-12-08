2980e2a94474c7c592496bfb21253fb15894b630
nns503 <73779581+nns503@users.noreply.github.com>
2022-12-08 14:44:08 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>	
+#include <string>
+#include <climits>
+
+using namespace std;
+
+int dp[1001];
+
+int solution(int x){
+	if(x==1) return 1;
+	if(x==2) return 3;
+	if(dp[x]!=-1) return dp[x];
+	dp[x] = (solution(x-1)+solution(x-2)*2)%10007;
+	return dp[x];
+}
+
+int main(void){
+	
+	int n;
+	cin >> n;
+	fill_n(dp,1001,-1);
+	cout << solution(n);
+	
+	
+	return 0;
+}
+
+
