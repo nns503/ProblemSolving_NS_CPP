c8f040e9d10ae3d87f9e69276ff49c8767a661a6
nns503 <73779581+nns503@users.noreply.github.com>
2023-02-01 11:27:38 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main(void){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);	
+	cout.tie(NULL);
+	int n;
+	cin >> n;
+	for(int i=0;i<n;i++){
+		int a;
+		cin >> a;
+		if(a==0){
+			if(pq.empty()) cout << 0 << "\n";
+			else{
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else pq.push(a);
+	}
+	return 0;
+}
