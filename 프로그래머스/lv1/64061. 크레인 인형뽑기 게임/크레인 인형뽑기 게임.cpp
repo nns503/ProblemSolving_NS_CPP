2a8411a7cf40b5c6620e992b52d041084ec5772d
nns503 <73779581+nns503@users.noreply.github.com>
2023-02-06 16:25:31 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.03 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int solution(vector<vector<int>> board, vector<int> moves) {
+    int answer = 0;
+    stack<int> s;
+    
+    for(int i=0;i<moves.size();i++){
+        int row = moves[i]-1;
+        
+        for(int j=0;j<board.size();j++){
+            
+            int target = board[j][row];
+            if(target!=0){
+                board[j][row]=0;
+                if(!s.empty()&&s.top()==target){
+                    s.pop();
+                    answer+=2;
+                    break;
+                }
+                else{
+                    s.push(target);
+                    break;
+                }                 
+            }
+            
+        }
+    }
+    
+    
+    return answer;
+}
+
