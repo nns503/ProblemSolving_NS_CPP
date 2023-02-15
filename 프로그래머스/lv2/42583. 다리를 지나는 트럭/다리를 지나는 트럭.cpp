1d3f1d05b7afd88d21c19208f57848189668daf0
nns503 <73779581+nns503@users.noreply.github.com>
2023-02-15 16:07:14 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -1,32 +1,42 @@
-#include <bits/stdc++.h>
-
+#include <iostream>
+#include<algorithm>
+#include <functional>         // greater 사용 위해 필요  
+#include <vector>
+#include<queue>
 using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
     int answer = 0;
-    int sum = 0;
-    queue<int> stand;
-    queue<int> bridge;
-    
-    for(auto a : truck_weights) stand.push(a);
-    for(int i=0;i<bridge_length;i++) bridge.push(0);
-    
-    while(!stand.empty()){
-        answer++;
-        sum -= bridge.front();
-        bridge.pop();
-        if(sum+stand.front()<=weight){
-            bridge.push(stand.front());
-            sum += stand.front();
-            stand.pop();
+    int count = 0;
+    int Time = 0; 
+    int Truck_weight = 0;
+    queue<pair<int, int>> truck_move;
+
+    while (true)
+    {
+        if (weight >= Truck_weight + truck_weights.at(count))
+        {
+            truck_move.push(make_pair(truck_weights.at(count), bridge_length + 1 + Time));
+            Truck_weight += truck_weights.at(count);
+            count++;
         }
-        else bridge.push(0);
+
+        if (count >= truck_weights.size())
+        {
+            answer = truck_move.back().second;
+            break;
+        }
+        else
+        {
+            Time++;
+            if (truck_move.front().second == Time+1)
+            {
+                Truck_weight -= truck_move.front().first;
+                truck_move.pop();
+            }
+        }
+
     }
-    
-    while(!bridge.empty()){
-        answer++;
-        bridge.pop();
-    }
-    
+
     return answer;
 }
