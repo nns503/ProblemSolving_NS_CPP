3977a916de6f26e4d74dbd83a388b74d286c8584
nns503 <73779581+nns503@users.noreply.github.com>
2023-02-20 10:57:18 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n,m;
+int num[8];
+int arr[8];
+bool chk[8];
+
+void func(int cnt, int order){
+	if(cnt==m){
+		for(int i=0;i<m;i++){
+			cout << arr[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	for(int i=0;i<n;i++){
+		if(chk[i]) continue;
+		if(i<order) continue;
+		arr[cnt]=num[i];
+		chk[i]=true;
+		func(cnt+1,i);
+		chk[i]=false;
+	}
+}
+
+int main(void){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> m;
+	for(int i=0;i<n;i++) cin >> num[i];
+	sort(num,num+n);
+	func(0,0);
+	return 0;
+}
