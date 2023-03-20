0eb40b926add83e7f0821e35d0ae3b0871ef1cf9
nns503 <73779581+nns503@users.noreply.github.com>
2023-03-20 16:38:50 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(int n) {
+    string answer = "";
+    for(int i=1; i<=n; i++){
+        if(i%2) answer += "수";
+        else answer += "박";
+    }
+    return answer;
+}
