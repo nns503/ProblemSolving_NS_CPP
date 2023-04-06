d10f743220241d9c288174f74022da5d43625a00
nns503 <73779581+nns503@users.noreply.github.com>
2023-04-06 12:10:01 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 95.90 ms, Memory: 20.3 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -6,10 +6,10 @@ unordered_set<int> us;
 
 int solution(vector<int> elements) {
     int answer = 0;
-    for(int len = 1; len <= elements.size(); len++){
-        for(int i=0; i<elements.size(); i++){
-            int a = 0;
-            for(int k = 0; k < len; k++) a += elements[(i+k) % elements.size()];
+    for(int i=0; i<elements.size(); i++){
+        int a = 0;
+        for(int j=i; j<elements.size() + i; j++){
+            a += elements[j % elements.size()];
             us.insert(a);
         }
     }
