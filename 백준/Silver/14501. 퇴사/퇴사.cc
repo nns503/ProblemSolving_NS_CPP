749202cd9c0b3903422d26a3a28695c6fa53af12
nns503 <73779581+nns503@users.noreply.github.com>
2023-04-24 16:53:44 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define X first
+#define Y second
+
+using namespace std;
+
+int dp[17];
+int n;
+
+int main(void){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> n;
+	
+	for(int i=1; i<=n; i++){
+		int t, p;
+		cin >> t >> p;
+		if(i+t-1 > n) continue;
+		dp[i] = max(dp[i], dp[i-1]);
+		dp[i+t] = max(dp[i+t], dp[i] + p);
+	}	
+	
+	cout << *max_element(dp, dp+17);
+	
+	return 0;
+}
