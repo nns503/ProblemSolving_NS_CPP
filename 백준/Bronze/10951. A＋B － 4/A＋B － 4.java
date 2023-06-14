0d182686175488cf1f8857ad98e0507c2d50c79b
nns503 <73779581+nns503@users.noreply.github.com>
2023-06-14 11:11:29 +0900

    [Bronze V] Title: A+B - 4, Time: 228 ms, Memory: 17868 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,15 @@
+// EOF 관련 문제
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        while(sc.hasNext()){
+            int Num_A = sc.nextInt();
+            int Num_B = sc.nextInt(); 
+            
+            System.out.println(Num_A+Num_B);
+        }
+    }
+}
