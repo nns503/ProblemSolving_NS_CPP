e127b34a12f8649caf463a0bf73705fe7bf4395e
nns503 <73779581+nns503@users.noreply.github.com>
2023-09-04 13:50:03 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+void func(int len){
+	if(len == 0) return;
+	
+	for(int i=0; i<len; i++){
+		cout << '*';
+	}
+	cout << '\n';
+	func(len - 1);
+}
+
+int main(void){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> n;
+	
+	func(n);	
+	
+	return 0;
+}
+
