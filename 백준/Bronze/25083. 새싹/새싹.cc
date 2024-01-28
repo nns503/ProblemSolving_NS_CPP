b73fb41ad76f6e948d560d3b1509f5e0aee12a0b
nns503 <73779581+nns503@users.noreply.github.com>
2024-01-28 19:32:02 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> str;
+int main(void){
+    
+    cout << ("         ,r'\"7") << '\n';
+    cout << ("r`-_   ,\'  ,/") << '\n';
+    cout << (" \\. \". L_r\'") << '\n';
+    cout << ("   `~\\/") << '\n';
+    cout << ("      |") << '\n' ;
+    cout << ("      |") << '\n' ;
+
+    for(auto cur : str){
+        cout << cur << '\n';
+    }
+    return 0;
+}
