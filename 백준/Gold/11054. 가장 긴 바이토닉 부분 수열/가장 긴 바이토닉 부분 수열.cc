7b46d8635d6d1b1b6be3c23906cf7f730cc17e11
nns503 <73779581+nns503@users.noreply.github.com>
2024-03-19 14:47:47 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
@@ -2,45 +2,58 @@
 
 using namespace std;
 
-int n;
+int asclen[1001];
+int desclen[1001];
+int n, a;
 vector<int> arr;
-int a[1001];
-int b[1001];
-int dp[1001];
 
 int main(void){
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	
-	cin >> n;
-	
-	for(int i=0; i<n; i++){
-		int z; cin >> z; arr.push_back(z);
-	}
-	
-	fill(a, a+n, 1); fill(b, b+n, 1);
-	
-	for(int i=1; i<n; i++){
-		for(int j=0; j<i; j++){
-			if(arr[j] < arr[i] && a[j] + 1 > a[i]){
-				a[i] = a[j] + 1;
-			}
-		} 
-	}
-	
-	for(int i=n-2; i>=0; i--){
-		for(int j=n-1; j>i; j--){
-			if(arr[j] < arr[i] && b[j] + 1 > b[i]){
-				b[i] = b[j] + 1;
-			}
-		} 
-	}
-	
-	for(int i=0; i<n; i++){
-		dp[i] = a[i] + b[i] - 1;
-	}
-	
-	cout << *max_element(dp, dp+n);
-	
-	return 0;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;   
+
+    for(int i=0; i<n; i++){
+        cin >> a;
+        arr.push_back(a);
+    }
+
+    vector<int> ascArr;
+    asclen[0] = 1; ascArr.push_back(arr[0]);
+
+    for(int i=1; i<n; i++){
+        int cur = arr[i];
+        if(ascArr.back() < cur){
+            ascArr.push_back(cur);
+            asclen[i] = asclen[i-1] + 1;
+        }
+        else{
+            int index = lower_bound(ascArr.begin(), ascArr.end(), cur) - ascArr.begin();
+            ascArr[index] = cur;
+            asclen[i] = asclen[i-1];
+        }
+    }
+
+    vector<int> descArr;
+    desclen[n-1] = 1; descArr.push_back(arr[n-1]);
+    
+    for(int i=n-2; i>=0; i--){
+        int cur = arr[i];
+        if(descArr.back() < cur){
+            descArr.push_back(cur);
+            desclen[i] = desclen[i+1] + 1;
+        }
+        else{
+            int index = lower_bound(descArr.begin(), descArr.end(), cur) - descArr.begin();
+            descArr[index] = cur;
+            desclen[i] = desclen[i+1];
+        }
+    }
+
+    int answer = 0;
+    for(int i=0; i<n; i++){
+        answer = max(answer, desclen[i] + asclen[i] - 1);
+    }
+    cout << answer;
+    return 0;
 }
