aa833e3db200d848aa26bb96049eefe083ee2aba
nns503 <73779581+nns503@users.noreply.github.com>
2024-04-10 18:10:39 +0900

    [Silver II] Title: 결혼식, Time: 0 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.cc
+++ 백준/Silver/5567. 결혼식/결혼식.cc
@@ -2,30 +2,41 @@
 
 using namespace std;
 
-int n, m;
-vector<int> v[501];
-bool vis[501];
-int answer = 0;
+int DFS(vector<vector<int>> &adj, int cur, int n){
+    int answer = 0;
+    stack<int> s; s.push(cur);
+    vector<int> dist(n+1, -1);
+    dist[cur] = 0;
+    while(!s.empty()){
+        cur = s.top(); s.pop();
+        for(auto nxt : adj[cur]){
+            if(dist[nxt] != -1) continue;
+            dist[nxt] = dist[cur] + 1;
+            answer++;
+            if(dist[nxt] == 1) s.push(nxt);
+        }
+    }
 
+    return answer;
+}
 
 int main(void){
-	cin >> n >> m;
-	for(int i=0; i<m; i++){
-		int a, b;
-		cin >> a >> b;
-		v[a].push_back(b);
-		v[b].push_back(a);
-	}
-	
-	vis[1] = true;
-	for(auto a : v[1]){
-		if(!vis[a]) answer++;		
-		vis[a] = true;
-		for(auto b : v[a]){
-			if(!vis[b]) answer++;
-			vis[b] = true;
-		}
-	}
-	cout << answer;
-	return 0;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T, n, m, a, b;
+    T = 1;
+    
+    while(T--){
+        cin >> n >> m;
+        vector<vector<int>> adj(n+1);
+        vector<int> dist(n+1, -1);
+        for(int i=0; i<m; i++){
+            cin >> a >> b;
+            adj[a].push_back(b);
+            adj[b].push_back(a);
+        }
+        cout << DFS(adj, 1, n) << '\n';
+    }
+    return 0;
 }
