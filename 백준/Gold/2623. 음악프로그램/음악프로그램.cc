80b27c1ca3adafa0ce51e8107a91f028da281cfe
nns503 <73779581+nns503@users.noreply.github.com>
2024-04-11 15:50:49 +0900

    [Gold III] Title: 음악프로그램, Time: 0 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Gold/2623. 음악프로그램/음악프로그램.cc
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.cc
@@ -2,47 +2,62 @@
 
 using namespace std;
 
-vector<int> adj[1001];
-int drg[1001];
-int n, m;
-queue<int> Q;
-vector<int> answer;
-
 int main(void){
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	
-	cin >> n >> m;
-	
-	for(int i=0; i<m; i++){
-		int t, a ,b;
-		cin >> t >> a;
-		t--;
-		while(t--){
-			cin >> b;
-			adj[a].push_back(b);
-			a = b; 
-			drg[b]++;
-		}
-	}
-	for(int i=1; i<=n; i++){
-		if(drg[i] == 0) Q.push(i);
-	}	
-	
-	while(!Q.empty()){
-		int cur = Q.front(); Q.pop();
-		answer.push_back(cur);
-		for(int nxt : adj[cur]){
-			drg[nxt]--;
-			if(drg[nxt] == 0) Q.push(nxt);
-		}
-	}
-	
-	if(answer.size() == n){
-		for(auto a : answer){
-			cout << a << '\n';
-		}
-	}
-	else cout << 0;
-	return 0;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T, n, m, a, b, cnt;
+    
+    T = 1;
+
+    while(T--){
+        cin >> n >> m;
+        vector<int> num(n+1);
+        vector<vector<int>> adj(n+1);
+        
+        for(int i=0; i<m; i++){
+            cin >> cnt;
+            cin >> a;
+            for(int k=1; k<cnt; k++){
+                cin >> b;
+                num[b]++;
+                adj[a].push_back(b);
+                a = b;
+            }
+        }
+
+        int number = 0;
+        queue<int> q;
+        vector<int> answer;
+        for(int i=n; i>=1; i--){
+            if(num[i] == 0) {
+                number++;
+                q.push(i);
+                answer.push_back(i);
+            }
+        }
+
+        while(!q.empty()){
+            auto cur = q.front(); q.pop();
+            for(auto nxt : adj[cur]){
+                num[nxt]--;
+                if(num[nxt] == 0){
+                    answer.push_back(nxt);
+                    q.push(nxt);
+                    number++;
+                }
+            }
+        }
+
+        if(number < n) cout << 0;
+        else{
+            for(auto cur : answer){
+                cout << cur << '\n';
+            }
+        }
+
+        
+
+    }
+    return 0;
 }
