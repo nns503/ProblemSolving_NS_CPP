d5a53c393ed67eefa59a51575c6fb1c3f75c9d97
nns503 <73779581+nns503@users.noreply.github.com>
2024-04-11 15:32:02 +0900

    [Gold IV] Title: 이분 그래프, Time: 248 ms, Memory: 5856 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.cc
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.cc
@@ -1,59 +1,50 @@
-	#include <iostream>
-	#include <string>
-	#include <vector>
-	#include <algorithm>
-	#include <queue>
-	
-	using namespace std;
-	
-	
-	void DFS(vector<int> graph[], int color[], int start){
-		if(color[start]==0) color[start] = 1;	
-		
-		for(int i=0;i<graph[start].size();i++){
-			int next = graph[start][i];
-			
-			if(color[next]==0){
-				if(color[start]==1) color[next]=2;
-				else if(color[start]==2) color[next]=1; 
-				DFS(graph,color,next);
-			}
-		}
-	}
-	
-	bool isBipartitegraph(vector<int> graph[], int color[], int n){
-		for(int i=1;i<=n;i++){
-			for(int j=0;j<graph[i].size();j++){
-				if(color[i]==color[graph[i][j]]){
-					return false;
-				}
-			}
-		}
-		return true;
-	}
-	
-	
-	int main(void){
-		int k; 
-		cin >> k;
-		
-		for(int i=0;i<k;i++){
-			vector<int> graph[20001];
-			int color[20001];
-			int n,m;
-			cin >> n >> m;
-			fill_n(color,n+1,0);	
-			for(int j=0;j<m;j++){
-				int u,v;
-				cin >> u >> v;
-				graph[u].push_back(v);
-				graph[v].push_back(u);
-			}
-			for(int j=1;j<=n;j++){
-				DFS(graph,color,j);
-			}
-			cout << (isBipartitegraph(graph,color,n)?"YES":"NO") << "\n" ;
-		}
-		
-		return 0;
-	}
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool BFS(vector<vector<int>> &adj, int n){
+    vector<int> vis(n+1, -1);
+    stack<int> s;
+    
+    for(int i=1; i<=n; i++){
+        if(vis[i] != -1) continue;
+        s.push(i);
+        vis[i] = 0;
+        while(!s.empty()){
+            auto cur = s.top(); s.pop();
+            for(auto nxt : adj[cur]){
+                if(vis[nxt] == -1){
+                    s.push(nxt);
+                    vis[nxt] = (vis[cur]+1) % 2;     
+                }
+                else{
+                    if(vis[cur] == vis[nxt]) return false;
+                    continue;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T, n, e, u, v;
+    cin >> T;
+
+    while(T--){
+        cin >> n >> e;
+        vector<vector<int>> adj(n+1);
+        for(int i=0; i<e; i++){
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        cout << (BFS(adj, n)?"YES":"NO") << '\n';
+        
+    }
+    return 0;
+}
