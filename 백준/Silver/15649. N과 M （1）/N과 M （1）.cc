3c3fe34167c69cc86cd9c7a3e4ade7b33282c80d
nns503 <73779581+nns503@users.noreply.github.com>
2024-05-12 19:59:33 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,31 +1,34 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int n,m;
-int number[9];
-int check[9];
-
-void DFS(int depth){
-    if(depth==m){
-        for(int i=0;i<m;i++){
-            cout << number[i] << " ";
+void func(vector<int> &arr, vector<bool> &vis, int cnt, int n, int m){
+    if(cnt == m){
+        for(auto cur : arr){
+            cout << cur << ' ';
         }
-        cout << "\n";
-        return;
+        cout << '\n';
+        return ;
     }
-    for(int i=1;i<=n;i++){
-        if(!check[i]){
-            check[i]=true;
-            number[depth] = i;
-            DFS(depth+1);
-            check[i] = false;
-        }
+
+    for(int i=1; i<=n; i++){
+        if(vis[i]) continue;
+        arr[cnt] = i;
+        vis[i] = true;
+        func(arr, vis, cnt+1, n, m);
+        vis[i] = false;
     }
 }
 
 int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, m;
     cin >> n >> m;
-    DFS(0);
+    
+    vector<int> arr(m);
+    vector<bool> vis(m, false);
+    func(arr, vis, 0, n, m);
     return 0;
 }
