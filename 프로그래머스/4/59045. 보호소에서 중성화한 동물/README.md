04c1bf09c3172de07031fb587ad125727cdb3190
나성 <73779581+nns503@users.noreply.github.com>
2025-02-02 20:55:03 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/README.md
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/README.md
@@ -1,6 +1,6 @@
 # [level 4] 보호소에서 중성화한 동물 - 59045 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59045) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59045?language=mysql) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 06월 24일 16:49:38
+2025년 02월 02일 20:54:58
 
 ### 문제 설명
 
--- 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,12 +1,9 @@
--- 코드를 입력하세요
-SELECT *
-from (
-    select ai.animal_id, ai.animal_type, ai.name
-    from animal_ins ai
-    where ai.sex_upon_intake like 'Intact%'
-) a
-where a.animal_id in (
-    select ao.animal_id
-    from animal_outs ao
-    where ao.sex_upon_outcome not like 'Intact%'
-) 
+select 
+    i.animal_id as animal_id,
+    i.animal_type as animal_type,
+    i.name as name
+from animal_ins i
+join animal_outs o on i.animal_id = o.animal_id
+where i.sex_upon_intake like 'Intact%'
+AND o.sex_upon_outcome not like 'Intact%'
+order by animal_id asc
