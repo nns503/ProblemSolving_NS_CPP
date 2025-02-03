c189be2e84a341b2a60600fb70e498192115e0a7
나성 <73779581+nns503@users.noreply.github.com>
2025-02-03 13:00:39 +0900

    [level 2] Title: 입양 시각 구하기(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59412. 입양 시각 구하기（1）/README.md
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/README.md
@@ -1,6 +1,6 @@
 # [level 2] 입양 시각 구하기(1) - 59412 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59412) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/59412?language=mysql) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 06월 24일 20:10:27
+2025년 02월 03일 13:00:37
 
 ### 문제 설명
 
--- 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
+++ 프로그래머스/2/59412. 입양 시각 구하기（1）/입양 시각 구하기（1）.sql
@@ -1,7 +1,8 @@
--- 코드를 입력하세요
-SELECT to_number(to_char(a.datetime, 'HH24')) as HOUR, count(*) as COUNT
-from animal_outs a
-group by to_char(a.datetime, 'HH24')
-having to_char(a.datetime, 'HH24') >= 9 
-AND to_char(a.datetime, 'HH24') <= 19
-order by to_char(a.datetime, 'HH24')
+select 
+    hour(datetime) as hour,
+    count(*) as count
+from animal_outs 
+where hour(datetime) >= 9 
+and hour(datetime) <= 19
+group by hour(datetime)
+order by hour
