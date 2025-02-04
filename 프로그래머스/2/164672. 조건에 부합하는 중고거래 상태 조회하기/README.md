32dc48f3db0a545516c6ee51dd3065b3ad66db7a
나성 <73779581+nns503@users.noreply.github.com>
2025-02-05 01:21:39 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/README.md
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 조건에 부합하는 중고거래 상태 조회하기 - 164672 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164672) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/164672?language=mysql) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2024년 06월 25일 16:03:31
+2025년 02월 05일 01:21:36
 
 ### 문제 설명
 
--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,10 +1,13 @@
--- 코드를 입력하세요
-SELECT u.board_id, u.writer_id, u.title, u.price, 
-case u.status
-when 'SALE' then '판매중'
-when 'RESERVED' then '예약중'
-when 'DONE' then '거래완료'
-end
-from used_goods_board u
-where u.created_date = to_date('2022-10-05', 'YYYY-MM-DD')
-order by u.board_id desc
+select 
+    board_id,
+    writer_id,
+    title,
+    price,
+    case
+    when status = 'sale' then '판매중'
+    when status = 'reserved' then '예약중'
+    when status = 'done' then '거래완료'
+    end 
+from used_goods_board
+where created_date = '2022-10-05'
+order by board_id desc
