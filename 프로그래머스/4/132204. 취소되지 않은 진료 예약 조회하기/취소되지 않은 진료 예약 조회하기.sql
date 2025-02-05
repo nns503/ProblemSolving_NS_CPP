b89e73dce53c9186a7a1ff6f29d85894d01086e9
나성 <73779581+nns503@users.noreply.github.com>
2025-02-05 22:36:49 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -0,0 +1,14 @@
+select 
+    a.apnt_no,
+    p.pt_name,
+    p.pt_no,
+    a.mcdp_cd,
+    d.dr_name,
+    a.apnt_ymd
+from appointment a
+join patient p on a.pt_no = p.pt_no
+join doctor d on a.mddr_id = d.dr_id
+where a.mcdp_cd = 'cs'
+and date_format(a.apnt_ymd, '%Y-%m-%d') = '2022-04-13'
+and a.apnt_cncl_yn = 'n'
+order by a.apnt_ymd asc
