0a01982ed1a01644ed28cedab797bc2282709a86
나성 <73779581+nns503@users.noreply.github.com>
2025-02-14 01:39:21 +0900

    [level 2] Title: 큰 수 만들기, Time: 2671.79 ms, Memory: 5.69 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/README.md
+++ 프로그래머스/2/42883. 큰 수 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 큰 수 만들기 - 42883 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883#) 
 
 ### 성능 요약
 
-메모리: 7.39 MB, 시간: 9.58 ms
+메모리: 5.69 MB, 시간: 2671.79 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 14일 01:25:06
+2025년 02월 14일 01:39:19
 
 ### 문제 설명
 
--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -1,38 +1,22 @@
 #include <bits/stdc++.h>
-#define A first
-#define B second
 
 using namespace std;
 
 string solution(string number, int k) {
     string answer = "";
-    vector<int> arr(number.size(), 0x3f3f3f3f);
-    stack<pair<char, int>> stk;
-    for(int i=0; i<number.size(); i++){
-        auto num = number[i];
-        if(stk.empty()){
-            stk.push({num, i});
-            continue;
+    int st = 0;
+    for(int i=0; i<number.size() - k; i++){
+        auto mx = number[st];
+        int mxId = st;
+        for(int j=st; j<=k+i; j++){
+            if(mx < number[j]){
+                mx = number[j];
+                mxId = j;
+            }
         }
-        while(!stk.empty() && stk.top().A < num){
-            auto cur = stk.top(); stk.pop();
-            arr[cur.B] = i - cur.B;
-        }
-        stk.push({num, i});
+        answer += mx;
+        st = mxId + 1;
     }
     
-    for(int i=0; i<number.size(); i++){
-        auto len = arr[i];
-        if(len <= k){
-            k--;
-        }
-        else{
-            answer.push_back(number[i]);
-        }
-    }
-    while(k > 0){
-        k--;
-        answer.pop_back();
-    }
     return answer;
 }
