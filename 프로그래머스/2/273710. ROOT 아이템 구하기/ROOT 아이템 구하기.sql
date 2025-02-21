717cb47d092d6d6f461066fd0ed8c90fa17c6bb7
나성 <73779581+nns503@users.noreply.github.com>
2025-02-22 00:24:54 +0900

    [level 2] Title: ROOT 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
+++ 프로그래머스/2/273710. ROOT 아이템 구하기/ROOT 아이템 구하기.sql
@@ -1,5 +1,8 @@
-select i.item_id as ITEM_ID, i.item_NAME as ITEM_NAME
+select
+    i.item_id,
+    i.item_name
 from item_info i
-join item_tree t on i.item_id = t.item_id
+join item_tree t
+on i.item_id = t.item_id
 where t.parent_item_id is null
 order by i.item_id asc
