36ac0cdfb27ca12d69b03d13f321ac335d8910e4
나성 <73779581+nns503@users.noreply.github.com>
2025-03-04 02:46:31 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,7 +1,8 @@
-select e.id
-from ecoli_data e
-left join ecoli_data p1 on e.parent_id = p1.id
-left join ecoli_data p2 on p1.parent_id = p2.id
-where p2.parent_id is null
-and p1.parent_id is not null
-order by e.id
+select
+    e1.id
+from ecoli_data e1
+left join ecoli_data e2 on e1.parent_id = e2.id
+left join ecoli_data e3 on e2.parent_id = e3.id
+where e3.id is not null
+and e3.parent_id is null
+order by e1.id asc
