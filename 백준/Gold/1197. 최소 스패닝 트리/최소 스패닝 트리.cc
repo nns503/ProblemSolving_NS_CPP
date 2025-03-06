325e7136f13310d9527ff1e59ca8b8f1b7a65845
나성 <73779581+nns503@users.noreply.github.com>
2025-03-07 02:24:30 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 36 ms, Memory: 4848 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -1,44 +1,48 @@
 #include <bits/stdc++.h>
-#define X first
-#define Y second
 
 using namespace std;
 
-int v, e;
-bool chk[10001];
-vector<pair<int ,int>> adj[100001];
-int cnt;
-int answer;
-priority_queue<pair<int, int>, vector<pair<int ,int>>, greater<pair<int, int>>> pq;
+vector<int> p(100001, 0);
+
+int find(int x){
+    if(x == p[x]) return x;
+    return p[x] = find(p[x]);    
+}
+
+bool uni(int a, int b){
+    a = find(a);
+    b = find(b);
+    if(a == b) return true;
+    if(a < b){
+        p[a] = b;
+    }
+    else p[b] = a;
+    return false;
+}
 
 int main(void){
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	
-	cin >> v >> e;
-	while(e--){
-		int a, b, c;
-		cin >> a >> b >> c;
-		adj[a].push_back({c, b});
-		adj[b].push_back({c, a});
-	}
-	
-	chk[1] = true;
-	for(auto a : adj[1]) pq.push({a.X, a.Y});
-	
-	while(cnt < v-1){
-		auto cur = pq.top(); pq.pop();
-		if(chk[cur.Y]) continue;
-		answer += cur.X;
-		cnt++;
-		chk[cur.Y] = true;
-		for(auto nxt : adj[cur.Y]){
-			if(chk[nxt.Y]) continue;
-			pq.push(nxt);
-		}
-	}
-	
-	cout << answer;
-	
-	return 0;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int v, e;
+    cin >> v >> e;
+    for(int i=1; i<=v; i++) p[i] = i;
+
+    vector<tuple<int, int, int>> adj;
+    for(int i=0; i<e; i++){
+        int a, b, c;
+        cin >> a >> b >> c;
+        adj.push_back({c, a, b});
+    }
+    sort(adj.begin(), adj.end());
+
+    int answer = 0;
+    for(auto cur : adj){
+        int c, a, b;
+        tie(c, a, b) = cur;
+        if(uni(a, b)) continue;
+        answer += c;
+    }
+
+    cout << answer;
 }
