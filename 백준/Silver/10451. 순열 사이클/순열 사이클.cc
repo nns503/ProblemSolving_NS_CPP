169bb4c7062b20e2bc30fee8c61421f232ba7fa3
나성 <73779581+nns503@users.noreply.github.com>
2025-05-30 13:29:57 +0900

    [Silver III] Title: 순열 사이클, Time: 64 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.cc
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.cc
@@ -1,49 +1,49 @@
-	#include <iostream>
-	#include <string>
-	#include <vector>
-	#include <algorithm>
-	#include <queue>
-	
-	using namespace std;
-	
-	
-	void DFS(vector<int> graph[], bool check[], int start){
-		check[start] = true;	
-		
-		for(int i=0;i<graph[start].size();i++){
-			int next = graph[start][i];
-			if(!check[next]){
-				DFS(graph,check,next);
-			}
-		}
-	}
-	int main(void){
-		int k;
-		cin >> k;
-		for(int t=0;t<k;t++){
-			int n;
-			cin >> n;
-			int cycle = 0;
-			vector<int> graph[n+1];
-			bool check[n+1];
-			fill_n(check,n+1,false);
-			for(int i=1;i<=n;i++){
-				int u;
-				cin >> u;
-				graph[i].push_back(u);
-				graph[u].push_back(i);
-			}
-			
-			for(int i=1;i<=n;i++){
-				if(check[i]==true) continue;
-				else{
-					cycle++;
-					DFS(graph,check,i);
-				}
-			}  
-			
-			cout << cycle << "\n";
-		} 	
-		
-		return 0;
-	}
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void dfs(int cur, vector<vector<int>> &adj, vector<bool> &vis){
+    for(auto nxt : adj[cur]){
+        if(vis[nxt]) continue;
+        vis[nxt] = true;
+        dfs(nxt, adj, vis);
+    }
+}
+
+int solve(int n, vector<int> &arr){
+    int count = 0;
+    vector<vector<int>> adj(n+1);
+
+    for(int i=0; i<n; i++){
+        adj[i+1].push_back(arr[i]);
+    }
+
+    vector<bool> vis(n+1, false);
+    for(int i=1; i<=n; i++){
+        if(vis[i]) continue;
+        vis[i] = true;
+        count++;
+        dfs(i, adj, vis);
+    }
+
+    return count;
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    while(T--){
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for(auto &cur : arr){
+            cin >> cur;
+        }
+        cout << solve(n, arr) << '\n'; 
+    }
+    return 0;
+}
