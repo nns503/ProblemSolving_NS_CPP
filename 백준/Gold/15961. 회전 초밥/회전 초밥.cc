ad9b87e80387d1f80406dc948700205e20360468
나성 <73779581+nns503@users.noreply.github.com>
2025-08-11 03:05:29 +0900

    [Gold IV] Title: 회전 초밥, Time: 248 ms, Memory: 13872 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.cc
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.cc
@@ -2,54 +2,46 @@
 
 using namespace std;
 
-vector<int> arr;
 int n, d, k, c;
-int vis[3001];
-int pos = 0;
-int st = 0;
-int cnt;
-int answer;
+
+int solve(vector<int> &arr){
+    int answer = -1;
+    vector<int> chobob(d+1, 0);
+    chobob[c] = 0x3f3f3f3f;
+    int cnt = 1; 
+
+    int st = 0;
+    for(int i=0; i<n+k; i++){
+        if(i - st + 1 > k){
+            int prv = arr[st % n];
+            chobob[prv]--;
+            if(chobob[prv] == 0){
+                cnt--;
+            }
+            st++;
+        }
+
+        int number = arr[i % n];
+        chobob[number]++;
+
+        if(chobob[number] == 1){
+            cnt++;
+        }
+        answer = max(answer, cnt);
+    }
+
+    return answer;
+}
 
 int main(void){
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	
-	cin >> n >> d >> k >> c;
-	
-	for(int i=0; i<n; i++){
-		int a;
-		cin >> a;
-		arr.push_back(a);
-	}
-	
-	for(int i=0; i<k; i++){
-		arr.push_back(arr[i]);
-	}
-	vis[c]++; cnt++;
-	
-	while(pos < arr.size()){
-		int cur = arr[pos];
-		int s = arr[st];
-		if(pos < k){
-			if(vis[cur] == 0) cnt++;
-			vis[cur]++;
-			pos++;
-		}
-		else{
-			if(s != c){
-				vis[s]--;
-				if(vis[s] == 0) cnt--;
-			}
-			if(cur != c){
-				vis[cur]++;
-				if(vis[cur] == 1) cnt++;	
-			}
-			pos++;
-			st++;
-		}
-		answer = max(answer, cnt);	
-	}
-	
-	cout << answer;
-	return 0;
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> d >> k >> c;
+    vector<int> arr(n);
+    for(auto &cur : arr) cin >> cur;
+
+    cout << solve(arr);
+
+    return 0;
 }
