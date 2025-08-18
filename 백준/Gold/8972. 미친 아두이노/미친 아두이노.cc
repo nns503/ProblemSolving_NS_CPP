54ad17f02308296cb2161f76576d7a2b658d4e63
나성 <73779581+nns503@users.noreply.github.com>
2025-08-18 18:50:53 +0900

    [Gold III] Title: 미친 아두이노, Time: 116 ms, Memory: 2692 KB -BaekjoonHub

--- 백준/Gold/8972. 미친 아두이노/미친 아두이노.cc
+++ 백준/Gold/8972. 미친 아두이노/미친 아두이노.cc
@@ -13,6 +13,86 @@ int getLen(int y, int x){
     return abs(y - jongsu.Y) + abs(x - jongsu.X);
 }
 
+vector<string> solve(vector<string> &board, string str){
+    vector<string> answer;
+    // 종수가 어딨는지 기록합니다.
+    for(int y=0; y<r; y++){
+        for(int x=0; x<c; x++){
+            if(board[y][x] == 'I'){
+                jongsu = {y, x};
+            }
+        }
+    }
+
+    for(int i=0; i<str.size(); i++){
+        int move = str[i] - '0'; // 움직임 방향 
+
+        // 1 + 2. 종수야 움직여
+        if(board[jongsu.Y + dy[move]][jongsu.X + dx[move]] == 'R'){ // 미친 아두이노로 가면 종료
+            answer.push_back("kraj " + to_string(i+1));
+            return answer;
+        }
+        else{ // 아니면 이동시킴
+            board[jongsu.Y][jongsu.X] = '.';
+            jongsu = {jongsu.Y + dy[move], jongsu.X + dx[move]};
+            board[jongsu.Y][jongsu.X] = 'I';
+        }
+    
+        map<pair<int, int>, int> cnt; // 해당 지점의 아두이노 개수
+        vector<string> tempBoard(r); 
+        for(int y=0; y<r; y++){
+            tempBoard[y] = board[y];
+        }
+        
+        // 아두이노 찾기
+        for(int y=0; y<r; y++){
+            for(int x=0; x<c; x++){
+                // 미친 아두이노인 경우
+                if(board[y][x] == 'R'){
+                    // 3.미친 아두이노는 종수한테 이동
+                    int temp = 0x3f3f3f3f; // 종수와의 거리
+                    pair<int, int> movePoint = {y, x}; // 미친 아두이노 이동 임시 위치
+                    for(int d=1; d<=9; d++){
+                        if(d == 5) continue;
+                        int ny = y + dy[d];
+                        int nx = x + dx[d];
+                        if(temp > getLen(ny, nx)){
+                            temp = getLen(ny, nx);
+                            movePoint.Y = ny;
+                            movePoint.X = nx;
+                        }
+                    }
+                    
+                    // 4. 종수가 있는 자리면 종료
+                    if(tempBoard[movePoint.Y][movePoint.X] == 'I'){
+                        answer.push_back("kraj " + to_string(i+1));
+                        return answer;
+                    }
+
+                    // 종수가 없으면 이동시키되, 그 해당 지점의 미친 아두이노 개수를 기록함
+                    else{
+                        tempBoard[movePoint.Y][movePoint.X] = 'R';
+                        cnt[movePoint]++;
+                    }
+                }
+            }
+        }
+
+        for(int y=0; y<r; y++){
+            for(int x=0; x<c; x++){
+                if(cnt[{y, x}] == 1){ // 한개면 미친 아두이노 배치
+                    board[y][x] = 'R';
+                }
+                else board[y][x] = '.'; // 아니면 그냥 . 처리 (2개 이상이면 폭파하기 때문) 
+            }
+        }
+        board[jongsu.Y][jongsu.X] = 'I'; // 방금 위에서 종수 위치 지워버림 ㅠㅠ
+    }
+
+    return board;
+
+}
+
 int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
@@ -23,80 +103,9 @@ int main(void){
     string str;
     cin >> str;
 
-    for(int y=0; y<r; y++){
-        for(int x=0; x<c; x++){
-            if(board[y][x] == 'I'){
-                jongsu = {y, x};
-            }
-        }
-    }
-
-    for(int i=0; i<str.size(); i++){
-        int move = str[i] - '0';
-        if(board[jongsu.Y + dy[move]][jongsu.X + dx[move]] == 'R'){
-            cout << "kraj " << i+1 << '\n';
-            return 0;
-        }
-        else{
-            board[jongsu.Y][jongsu.X] = '.';
-            jongsu = {jongsu.Y + dy[move], jongsu.X + dx[move]};
-            board[jongsu.Y][jongsu.X] = 'I';
-        }
-        queue<pair<int, int>> q;
-        map<pair<int, int>, int> m;
-        vector<string> tempBoard(r);
-        for(int y=0; y<r; y++){
-            tempBoard[y] = board[y];
-        }
-        
-
-        for(int y=0; y<r; y++){
-            for(int x=0; x<c; x++){
-                if(board[y][x] == 'R'){
-                    int temp = 0x3f3f3f3f;
-                    int my = y;
-                    int mx = x;
-                    for(int d=1; d<=9; d++){
-                        if(d == 5) continue;
-                        int ny = y + dy[d];
-                        int nx = x + dx[d];
-                        if(temp > getLen(ny, nx)){
-                            temp = getLen(ny, nx);
-                            my = ny;
-                            mx = nx;
-                        }
-                    }
-                    if(tempBoard[my][mx] == 'I'){
-                        cout << "kraj " << i+1 << '\n';
-                        return 0;
-                    }
-                    else{
-                        tempBoard[my][mx] = 'R';
-                        m[{my, mx}]++;
-                    }
-                }
-            }
-        }
-
-        for(int y=0; y<r; y++){
-            for(int x=0; x<c; x++){
-                if(m[{y, x}] == 1){
-                    board[y][x] = 'R';
-                }
-                else if(m[{y, x}] >= 2){
-                    board[y][x] = '.';
-                }
-                else board[y][x] = '.';
-            }
-        }
-        board[jongsu.Y][jongsu.X] = 'I';     
-    }
-
-    for(int y=0; y<r; y++){
-        for(int x=0; x<c; x++){
-            cout << board[y][x];
-        }
-        cout << '\n';
+    vector<string> answer = solve(board, str);
+    for(auto cur : answer){
+        cout << cur << '\n';
     }
 
     return 0;
