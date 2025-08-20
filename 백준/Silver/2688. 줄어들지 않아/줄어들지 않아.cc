34fb846a02de00de8d3b40f3b08cf1863f668102
나성 <73779581+nns503@users.noreply.github.com>
2025-08-20 16:28:08 +0900

    [Silver I] Title: 줄어들지 않아, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2688. 줄어들지 않아/줄어들지 않아.cc
+++ 백준/Silver/2688. 줄어들지 않아/줄어들지 않아.cc
@@ -2,32 +2,39 @@
 
 using namespace std;
 
-int T;
-int n;
-long long dp[65][11];
-long long sum[65];
+long long solve(int n){
+    vector<long long> dp(10, 1);
+    vector<long long> sum(n+1, 0);
+    
+    sum[1] = 10;
+
+    for(int i=2; i<=n; i++){
+        for(int num=9; num>=0; num--){
+            for(int k=0; k<num; k++){
+                dp[num] += dp[k];
+            }
+        }
+        
+        for(int num=0; num<=9; num++){
+            sum[i] += dp[num];
+        }
+    }
+
+    return sum[n];
+}
 
 int main(void){
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	
-	sum[1] = 10;
-	for(int i=1; i<=64; i++){
-		for(int k=0; k<=9; k++){
-			if(k == 0 || i == 1) dp[i][k] = 1;
-			else{
-				dp[i][k] = dp[i][k-1] + dp[i-1][k];
-			}
-			sum[i+1] += dp[i][k] * (10 - k);
-		}
-	}
-	
-	cin >> T;
-	
-	while(T--){
-		cin >> n;
-		cout << sum[n] << '\n';
-	}
-	
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+
+    while(T--){
+        int n;
+        cin >> n;
+        cout << solve(n) << '\n';
+    }
+
+    return 0;
 }
- 
