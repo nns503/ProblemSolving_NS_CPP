9bf3727d3dc1d9c5a35032a1e42dd1a03715a265
나성 <73779581+nns503@users.noreply.github.com>
2025-09-16 16:01:10 +0900

    [level 3] Title: 야근 지수, Time: 28.59 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.cpp
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.cpp
@@ -6,15 +6,21 @@ priority_queue<int> pq;
 
 long long solution(int n, vector<int> works) {
     long long answer = 0;
-    for(int a : works) pq.push(a);
-    while(n-- && !pq.empty()){
-        int cur = pq.top(); pq.pop();
-        if(cur-1 != 0) pq.push(cur-1);
+    
+    for(auto cur : works){
+        pq.push(cur);
+    }
+    
+    while(n-- > 0){
+        auto cur = pq.top(); pq.pop();
+        if(cur == 0) break;
+        pq.push(cur-1);
     }
     
     while(!pq.empty()){
-        long long cur = pq.top(); pq.pop();
+        auto cur = pq.top(); pq.pop();
         answer += cur * cur;
     }
+    
     return answer;
 }
